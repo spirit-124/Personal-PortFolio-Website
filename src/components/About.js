e3a760f7a9d5246dd992846d36d158b6e2135d42
Bajrang Agrawal <bajrangofficial580@gmail.com>
2023-02-01 02:20:44 +0530

    [completed] About Page

--- src/components/About.js
+++ src/components/About.js
@@ -15,11 +15,77 @@ const About = () => {
   return (
     <section className="section" id="about" ref={ref}>
       <div className="container mx-auto">
-        <div>
+        <div
+          className="flex flex-col gap-y-10 lg:flex-row lg:items-center lg:gap-x-20 lg:gap-y-0
+        h-screen"
+        >
           {/* img  */}
-          <div className="flex-1 bg-about bg-contain bg-no-repeat h-[640px] mix-blend-lighten bg-top"></div>
+          <motion.div
+            variants={fadeIn("right", 0.3)}
+            initial="hidden"
+            whileInView={"show"}
+            viewport={{ once: false, amount: 0.3 }}
+            className="flex-1 bg-about bg-contain bg-no-repeat h-[640px] mix-blend-lighten bg-top"
+          ></motion.div>
           {/* text  */}
-          <div className="text"></div>
+          <motion.div
+            variants={fadeIn("left", 0.5)}
+            initial="hidden"
+            whileInView={"show"}
+            viewport={{ once: false, amount: 0.3 }}
+            className="flex-1"
+          >
+            <h2 className=" h2 text-accent">About me.</h2>
+            <h3 className="mb-4 h3">
+              I'm a Freelancer Front-end Developer with over 5 years of
+              experience.
+            </h3>
+            <p className=" mb-6">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste
+              rerum soluta nulla voluptatibus nisi neque ducimus voluptatem sit
+              repellat impedit mollitia accusamus, molestiae aperiam dolor.{" "}
+            </p>
+
+            {/* stats  */}
+            <div className="flex gap-x-6 lg:gap-x-10 mb-12">
+              <div>
+                <div className="text-[40px] font-tertiary text-gradient mb-2">
+                  {inView ? <CountUp start={0} end={13} duration={3} /> : null}
+                </div>
+                <div className="font-primary text-sm tracking-[2px] ">
+                  Years of <br />
+                  Experience
+                </div>
+              </div>
+              <div>
+                <div className="text-[40px] font-tertiary text-gradient mb-2">
+                  {inView ? <CountUp start={0} end={15} duration={3} /> : null}
+                  k+
+                </div>
+                <div className="font-primary text-sm tracking-[2px] ">
+                  Projects
+                  <br />
+                  Completed
+                </div>
+              </div>
+              <div>
+                <div className="text-[40px] font-tertiary text-gradient mb-2">
+                  {inView ? <CountUp start={0} end={12} duration={3} /> : null}
+                  k+
+                </div>
+                <div className="font-primary text-sm tracking-[2px] ">
+                  satisfied <br />
+                  Customers
+                </div>
+              </div>
+            </div>
+            <div className="flex gap-x-8 items-center">
+              <button className="btn btn-lg">Contact Me</button>
+              <a href="#" className=" text-gradient btn-link">
+                My Portfolio
+              </a>
+            </div>
+          </motion.div>
         </div>
       </div>
     </section>
